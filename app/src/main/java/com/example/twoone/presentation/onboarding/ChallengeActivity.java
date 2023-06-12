82b67294ea020fba0e0a7bff28aa08bdafe67005
Sohyun <so405@naver.com>
2023-06-13 00:11:07 +0900

    [FEAT] fix edit text attribute

--- app/src/main/java/com/example/twoone/presentation/onboarding/ChallengeActivity.java
+++ app/src/main/java/com/example/twoone/presentation/onboarding/ChallengeActivity.java
@@ -3,6 +3,7 @@ package com.example.twoone.presentation.onboarding;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,7 @@ public class ChallengeActivity extends AppCompatActivity {
         binding = ActivityChallengeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //edit text 글자수 세는 리스너
         binding.etChallengeInput.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
